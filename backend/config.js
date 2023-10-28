6bf5239a15de96c8e95e640a8d68e9bd68d9bbb5
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2023-10-28 17:56:46 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://admin:admin@cluster0.clfwuwm.mongodb.net/?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
+  'mongodb+srv://admin:nimda@cluster0.clfwuwm.mongodb.net/?retryWrites=true&w=majority';
